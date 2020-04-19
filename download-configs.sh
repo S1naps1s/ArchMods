7640fef1bd04ec8c27762e09360aa1994098fc25
S1naps1s <57961601+S1naps1s@users.noreply.github.com>
2020-04-19 00:47:40 -0400

    Update download-configs.sh

--- download-configs.sh
+++ download-configs.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
-#
-################################
-# Written by: Ben Friend       #
-# Last Updated: April 17, 2020 #
-################################
+
+#################################
+#	Written by: Ben Friend		#
+#	Written: April 17, 2020		#
+#	Last Updated: April 19,2020	#
+#################################
+
+read -sp "Enter password for sudo: " sudoPW
+
+package=cmatrix
+if pacman -Qs $package > /dev/null ; then
+	echo -e "\n"$package "is installed"
+else
+	echo -e "\n"
+	echo $sudoPW | sudo -S pacman -S --noconfirm cmatrix
+fi
+package=figlet
+if pacman -Qs $package > /dev/null ; then
+	echo -e "\n"$package "is installed"
+else
+	echo -e "\n"
+	echo $sudoPW | sudo -S pacman -S --noconfirm figlet
+fi
+package=fortune-mod
+if pacman -Qs $package > /dev/null ; then
+	echo -e "\n"$package "is installed"
+else
+	echo -e "\n"
+	echo $sudoPW | sudo -S pacman -S --noconfirm fortune-mod
+fi
+package=neofetch
+if pacman -Qs $package > /dev/null ; then
+	echo -e "\n"$package "is installed"
+else
+	echo -e "\n"
+	echo $sudoPW | sudo -S pacman -S --noconfirm neofetch
+fi
+package=sddm
+if pacman -Qs $package > /dev/null ; then
+	echo -e "\n"$package "is installed"
+else
+	echo -e "\n"
+	echo $sudoPW | sudo -S pacman -S --noconfirm sddm
+fi
 
 if [ ! -d $HOME"/Downloads/GitConfigs/" ]; then
 	mkdir $HOME"/Downloads/GitConfigs/"
@@ -18,16 +57,15 @@ git pull ArchMods
 count=0
 count=$[count+1]
 yes | mv -f $HOME/Downloads/GitConfigs/ArchMods/.bashrc.conf ${HOME}/.bashrc
-echo "Config "$count ": " ${HOME}"/.bashrc done"
+echo "Config "$count ":" ${HOME}"/.bashrc done"
 
 count=$[count+1]
 mv -f $HOME/Downloads/GitConfigs/ArchMods/neofetch.conf ${HOME}/.config/neofetch/config.conf
-echo "Config "$count ": " ${HOME}"/.config/neofetch/config.conf done"
+echo "Config "$count ":" ${HOME}"/.config/neofetch/config.conf done"
 
 count=$[count+1]
-read -s -p "Enter password for sudo: " sudoPW
-echo $sudoPW | sudo mv -f $HOME/Downloads/GitConfigs/ArchMods/sddm.conf /etc/sddm.conf
+echo $sudoPW | sudo -S mv -f $HOME/Downloads/GitConfigs/ArchMods/sddm.conf /etc/sddm.conf
 echo "Config "$count ": /etc/sddm.conf done"
 
-chmod 777 $HOME"/Downloads/GitConfigs/"
-rm -Rf $HOME"/Downloads/GitConfigs/"
+echo $sudoPW | sudo -S chmod 777 $HOME"/Downloads/GitConfigs/"
+echo $sudoPW | sudo -S rm -Rf $HOME"/Downloads/GitConfigs/"
